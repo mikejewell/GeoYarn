1191fa0acee8048627052bf49bb064afaa4c96c5
Michael Jewell <moj@ip-204-135.eduroam.soton.ac.uk>
2012-09-14 18:58:43 +0100

    Cordova bits

--- src/uk/ac/soton/ecs/geoyarn/MainActivity.java
+++ src/uk/ac/soton/ecs/geoyarn/MainActivity.java
@@ -1,20 +1,13 @@
 package uk.ac.soton.ecs.geoyarn;
 
+import org.apache.cordova.*;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
